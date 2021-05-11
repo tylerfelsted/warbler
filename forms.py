474cb5f29de74998f4cb5886e1e31cfb869759d1
Tyler <tyler.felsted@yahoo.com>
2021-05-11 18:35:39 -0400

    added edit user functionality

--- forms.py
+++ forms.py
@@ -17,6 +17,16 @@ class UserAddForm(FlaskForm):
     password = PasswordField('Password', validators=[Length(min=6)])
     image_url = StringField('(Optional) Image URL')
 
+class UserEditForm(FlaskForm):
+    """Form for editing users"""
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    image_url = StringField('(Optional) Image URL')
+    header_image_url = StringField('(Optional) Header Image URL')
+    bio = TextAreaField('(Optional) Bio')
+    location = StringField('(Optional) Location')
+    password = PasswordField('Password', validators=[Length(min=6)])
+
 
 class LoginForm(FlaskForm):
     """Login form."""
