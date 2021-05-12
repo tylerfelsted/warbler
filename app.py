a8da3309db14143f2dae4b34192291e7bddbcbb1
Tyler <tyler.felsted@yahoo.com>
2021-05-11 23:14:53 -0400

    fixed homepage showing all messages. Now only shows messages from followed users

--- app.py
+++ app.py
@@ -315,6 +315,7 @@ def homepage():
     if g.user:
         messages = (Message
                     .query
+                    .filter(Message.user_id.in_([user.id for user in g.user.following]))
                     .order_by(Message.timestamp.desc())
                     .limit(100)
                     .all())
